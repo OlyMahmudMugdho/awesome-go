ae9e0c33e005565c02fcb11c476e65fbd5cede74
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-02-22 18:01:59 +0600

    checking type with reflect.TypeOf()

--- 4-types/main.go
+++ 4-types/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main(){
 	var age  int = 23;
 	var user string = "Mugdho"
 
+	const pi float32 = 3.14
+
 	fmt.Printf("The type of age is : %T \n", age)
 	fmt.Printf("The type of user is : %T \n", user)
+
+	fmt.Println("The type of pi is : ", reflect.TypeOf(pi))
 }
